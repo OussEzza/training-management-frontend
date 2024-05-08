666b97a0435c6d5545eec6f5cf4f000edc2f033a
OussEzza <ezzahrioussama01@gmail.com>
2024-05-08 10:56:36 +0100

    Upadted AuthView page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("define").tap((definitions) => {
+      definitions[0]["__VUE_PROD_HYDRATION_MISMATCH_DETAILS__"] =
+        JSON.stringify(true);
+      return definitions;
+    });
+  },
+});
