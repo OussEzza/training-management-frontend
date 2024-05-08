17d7070b6f14472ad1b30871cfe1721e20299a60
OussEzza <ezzahrioussama01@gmail.com>
2024-05-08 11:22:43 +0100

    Another update in Authentication page

--- src/components/AuthView.vue
+++ src/components/AuthView.vue
@@ -8,68 +8,31 @@
     />
     <h1>Login</h1>
     <form @submit.prevent="login" class="mb-4">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label"
-            >Email address</label
-          >
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            v-model="loginData.email"
-            required
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            v-model="loginData.password"
-            required
-          />
-        </div>
-
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-    <h1>Register</h1>
-    <form @submit.prevent="register">
       <div class="mb-3">
-        <label for="name" class="form-label">Name:</label>
-        <input
-          type="text"
-          v-model="registerData.name"
-          class="form-control"
-          id="name"
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label for="emailReg" class="form-label">Email:</label>
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
         <input
           type="email"
-          v-model="registerData.email"
           class="form-control"
-          id="emailReg"
-          required
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="loginData.email"
         />
+        <div v-if="errors.email" class="text-danger">{{ errors.email }}</div>
       </div>
       <div class="mb-3">
-        <label for="passwordReg" class="form-label">Password:</label>
+        <label for="exampleInputPassword1" class="form-label">Password</label>
         <input
           type="password"
-          v-model="registerData.password"
           class="form-control"
-          id="passwordReg"
-          required
+          id="exampleInputPassword1"
+          v-model="loginData.password"
         />
+        <div v-if="errors.password" class="text-danger">
+          {{ errors.password }}
+        </div>
       </div>
-      <button type="submit" class="btn btn-primary">Register</button>
+
+      <button type="submit" class="btn btn-primary">Login</button>
     </form>
   </div>
 </template>
@@ -84,15 +47,34 @@ export default {
         email: "",
         password: "",
       },
-      registerData: {
-        name: "",
-        email: "",
-        password: "",
+      errors: {
+        email: null,
+        password: null,
       },
     };
   },
   methods: {
     async login() {
+      // Reset errors
+      this.errors = {
+        email: null,
+        password: null,
+      };
+
+      // Perform validation
+      if (!this.loginData.email) {
+        this.errors.email = "Email is required";
+      }
+      if (!this.loginData.password) {
+        this.errors.password = "Password is required";
+      }
+
+      // If there are errors, stop login process
+      if (this.errors.email || this.errors.password) {
+        return;
+      }
+
+      // If no errors, proceed with login
       try {
         const response = await axios.post("http://127.0.0.1:8000/api/login", {
           email: this.loginData.email,
@@ -103,22 +85,6 @@ export default {
         this.$router.push("/");
       } catch (error) {
         console.error("Login error:", error.response.data);
-      }
-    },
-
-    async register() {
-      try {
-        const response = await axios.post(
-          "http://127.0.0.1:8000/api/register",
-          this.registerData
-        );
-        console.log(response.data);
-        // Optionally, you can automatically login the user after registration
-        // this.loginData.email = this.registerData.email;
-        // this.loginData.password = this.registerData.password;
-        // this.login();
-      } catch (error) {
-        console.error("Registration error:", error.response.data.errors);
       }
     },
   },
