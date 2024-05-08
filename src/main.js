8de08937c9c6277a2e17895e42da58280c4b05b0
OussEzza <ezzahrioussama01@gmail.com>
2024-05-08 09:35:24 +0100

    Added AuthVue component with he routes link

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Index from "./components/TrainingView.vue";
+import Edit from "./components/EditTraining.vue";
+import Login from "./components/AuthView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    name: "Login",
+    path: "/login",
+    component: Login,
+  },
+  {
+    name: "Index",
+    path: "/trainings",
+    component: Index,
+  },
+  {
+    name: "Edit",
+    path: "/trainings/edit/:id",
+    component: Edit,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
